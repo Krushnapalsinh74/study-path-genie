397d12e3bfec3def0351b47c4e6d5ac24da0ca5f
Krushnapalsinh74 <kp735926@gppalanpur.gujgov.edu.in>
2025-10-04 11:12:41 +0530

    done

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.study.pathgenie',
   appName: 'study-path-genie',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
